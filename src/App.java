8de0a6ed5ecf2feb06c3eb27268e68af42e9f5b4
voltex52 <voltex7@yandex.ru>
2022-11-03 19:38:30 +0300

    3rd commit

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello World again");
+        System.out.println("Hello World 3rd time");
     }
 }
